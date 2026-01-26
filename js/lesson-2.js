bc6ed2c63a9c6c3bbee487b3187058607329df92
maryana-frydrak <frydra4ka@gmail.com>
2026-01-27 00:04:54 +0200

    add 6 task

--- js/lesson-2.js
+++ js/lesson-2.js
@@ -8,5 +8,141 @@
 // Використайте цикл for для перебору елементів масиву.
 // Нумерація елементів повинна починатися з 1 (а не з 0).
 
-const newArray = ["jazz", "blues"];
-newArray.push("rock-n-roll");
+// const newArray = ['jazz', 'blues'];
+// newArray.push('rock-n-roll');
+// const index = newArray.indexOf('blues');
+// // newArray[index] = 'classic';// first metod
+// newArray.splice(index, 1, 'classic');// second metod
+
+// console.log(newArray);
+
+// function logItems(array) {
+//     for (let i = 0; i < array.length; i++) {
+//         console.log(`${i + 1} - ${array[i]}`);
+//     }
+// }
+
+// logItems(newArray);
+
+//! ------------------------------------------------------
+
+// Напишіть функцію checkLogin(array), яка:
+// Приймає масив логінів як аргумент.
+// Запитує ім'я користувача через prompt.
+// Перевіряє, чи є введене ім'я у переданому масиві.
+// Якщо ім'я є в масиві – виводить повідомлення через alert: "Welcome, <name>!"
+// Якщо ім'я відсутнє – виводить повідомлення: "User not found".
+
+// const logins = ["Peter", "John", "Igor", "Sasha"];
+
+// function checkLogin(array) {
+//     const userName = prompt("Введіть login");
+//     if (array.includes(userName)) {
+//         alert(`Welcome, ${userName}!`);
+//     } else {
+//         alert("User not found");
+//     }
+// }
+
+// checkLogin(logins);
+
+//! ---------------------------------------------------------------
+
+// Напишіть функцію caclculateAverage(),
+// яка приймає довільну кількість
+// аргументів і повертає їхнє середнє значення.
+// Додайте перевірку, що аргументи - це числа.
+
+// function caclculateAverage() {
+
+//     let sum = 0;
+//     let count = 0;
+
+//     for (const arg of arguments) {
+//         if (typeof arg === "number") {
+//             sum += arg;
+//             count += 1;
+//         }
+//     }
+//     return count === 0 ? 0 : sum / count;
+// }
+
+// console.log(caclculateAverage(1, 2, 3, true, "apple"));
+
+//! ----------------------------------------------------------
+
+// Напишіть функцію, яка сумуватиме сусідні числа
+// і пушитиме їх в новий масив.
+// уточнення: складати необхідно перше число з другим, потім друге - з третім,
+// третє - з четвертим і так до кінця.
+// В результаті функція має повертати масив [33, 45, 39, 17, 25, 27, 29].
+
+// const someArr = [22, 11, 34, 5, 12, 13, 14, 15];
+
+// function adjacentNumbersSum(array) {
+//     const result = [];
+//     for (let i = 0; i < array.length - 1; i++) {
+//         result.push(array[i] + array[i + 1]);
+//     }
+//     return result;
+// }
+
+// console.log(adjacentNumbersSum(someArr));
+
+//! --------------------------------------------------------
+
+// Напишіть функцію findSmallestNumber(numbers),
+// яка шукає найменше число в масиві.
+// Додайте перевірку, що функція отримує саме масив, і
+// якщо функція отримує масив - поверніть з функції найменше число,
+// в іншому випадку - поверніть 'Sory, it is not an array!'.
+
+// const numbers = [2, 5, 35, 56, 12, 24, 7, 80, 3];
+
+// function findSmallestNumber(array) {
+//     if (!Array.isArray(array)) {
+//         return 'Sory, it is not an array!';
+//     } else {
+//         return Math.min(...array);
+//     }
+// }
+
+// console.log(findSmallestNumber(numbers));
+
+//! -------------------------------------------------------------------
+
+// Напишіть функцію findLongestWord(string), яка
+// приймає довільний рядок, що складається лише зі слів, розділених
+// пробілами (параметр string), і повертатиме найдовше слово у реченні.
+
+// Скористайтесь цим прикладом виклику функції для перевірки її роботи:
+
+
+// function findLongestWord(string) {
+//     const words = string.split(" ");
+//     let longestWord = words[0];
+//     for (const word of words) {
+//         if (word.length > longestWord.length) {
+//             longestWord = word;
+//         }
+//     }
+//     return longestWord;
+// }
+
+// console.log(findLongestWord("London is the capital of Great Britain")); // 'capital'
+
+//! ----------------------------------------------------------------------------------------
+
+// Напишіть скрипт, який для об'єкту user, послідовно:
+// 1 - додасть поле mood зі значенням 'happy',
+// 2 - замінить hobby на 'skydiving',
+// 3 - замінить значення premium на false,
+// 4 - виведе зміст об'єкта user у форматі
+// '<ключ>:<значення>' використовуя Object.keys() та for...of
+
+// const user = {
+//     name: "John",
+//     age: 20,
+//     hobby: "tenis",
+//     premium: true,
+//   };
