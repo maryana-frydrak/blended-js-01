f4ed18a9e06ebde7430543f2a7ec2915653572a6
maryana-frydrak <frydra4ka@gmail.com>
2026-02-23 00:23:27 +0200

    add third-task

--- js/lesson-4.js
+++ js/lesson-4.js
@@ -19,88 +19,88 @@
 // 19 - зроби це саме, але використовуй шаблонні рядки та метод insertAdjacentHTML()
 // 20 - очисти список
 
-// // // !1 ============================================
-const body = document.querySelector("body");
+// !1 =========================================================================
+// const body = document.querySelector("body");
 
-console.log(body);
+// console.log(body);
 
-// !2 -----------------------------------------------------
-const titleId = document.getElementById("title");
+// // !2 -----------------------------------------------------
+// const titleId = document.getElementById("title");
 
-console.log(titleId);
+// console.log(titleId);
 
-// // !3 -----------------------------------------------------
-const list = document.querySelector(".list");
+// // // !3 -----------------------------------------------------
+// const list = document.querySelector(".list");
 
-console.log(list);
+// console.log(list);
 
-// // !4 ----------------------------------------------------------
-const byAllAtribute = document.querySelectorAll("[data-topic]");
+// // // !4 ----------------------------------------------------------
+// const byAllAtribute = document.querySelectorAll("[data-topic]");
 
-console.log(byAllAtribute);
+// console.log(byAllAtribute);
 
-// !5 ----------------------------------------------------------
-const firstbyAllAtribute = document.querySelector("[data-topic]");
+// // !5 ----------------------------------------------------------
+// const firstbyAllAtribute = document.querySelector("[data-topic]");
 
-console.log(firstbyAllAtribute);
+// console.log(firstbyAllAtribute);
 
-// !6 --------------------------------------------------------------
-// const lastbyAllAtribute = byAllAtribute[byAllAtribute.length - 1];
+// // !6 --------------------------------------------------------------
+// // const lastbyAllAtribute = byAllAtribute[byAllAtribute.length - 1];
 
-// console.log(lastbyAllAtribute); // один спосіб
+// // console.log(lastbyAllAtribute); // один спосіб
 
-const lastbyAllAtribute = list.lastElementChild;
+// const lastbyAllAtribute = list.lastElementChild;
 
-console.log(lastbyAllAtribute); // інший спосіб
+// console.log(lastbyAllAtribute); // інший спосіб
 
-// !7 ------------------------------------------------------------------
-const neighborEl = titleId.nextElementSibling;
+// // !7 ------------------------------------------------------------------
+// const neighborEl = titleId.nextElementSibling;
 
-console.log(neighborEl);
+// console.log(neighborEl);
 
-// !8 -----------------------------------------------------
-const h3 = document.querySelectorAll("h3");
+// // !8 -----------------------------------------------------
+// const h3 = document.querySelectorAll("h3");
 
-console.log(h3);
+// console.log(h3);
 
-// !9 -----------------------------------------------------
-h3.forEach((el) => el.classList.add("active"));
+// // !9 -----------------------------------------------------
+// h3.forEach((el) => el.classList.add("active"));
 
-// !10 -----------------------------------------------------------------
-const navigation = document.querySelector("[data-topic = navigation]");
+// // !10 -----------------------------------------------------------------
+// const navigation = document.querySelector("[data-topic = navigation]");
 
-console.log(navigation);
+// console.log(navigation);
 
-// !11 ------------------------------------------------------------------
-navigation.style.backgroundColor = "yellow";
+// // !11 ------------------------------------------------------------------
+// navigation.style.backgroundColor = "yellow";
 
-// !12 ------------------------------------------------------------------
-navigation.querySelector("p").textContent = "Я змінив тут текст!";
+// // !12 ------------------------------------------------------------------
+// navigation.querySelector("p").textContent = "Я змінив тут текст!";
 
-// !13 ------------------------------------------------------------------
-const currentTopic = "manipulation";
-const elem = document.querySelector(`[data-topic = ${currentTopic}]`);
+// // !13 ------------------------------------------------------------------
+// const currentTopic = "manipulation";
+// const elem = document.querySelector(`[data-topic = ${currentTopic}]`);
 
-console.log(elem);
+// console.log(elem);
 
-// !14 -------------------------------------------------------------------
-elem.style.backgroundColor = "blue";
+// // !14 -------------------------------------------------------------------
+// elem.style.backgroundColor = "blue";
 
-// !15 -------------------------------------------------------------------
-const head = document.querySelector(".completed");
+// // !15 -------------------------------------------------------------------
+// const head = document.querySelector(".completed");
 
-console.log(head);
+// console.log(head);
 
-// !16 --------------------------------------------------------------------
-head.parentNode.remove();
+// // !16 --------------------------------------------------------------------
+// head.parentNode.remove();
 
-// !17 --------------------------------------------------------------------
-const h1 = document.querySelector("h1");
-const ppp = document.createElement("p");
-ppp.textContent = "Об'єктна модель документа (Document Object Model)";
-h1.after(ppp);
+// // !17 --------------------------------------------------------------------
+// const h1 = document.querySelector("h1");
+// const ppp = document.createElement("p");
+// ppp.textContent = "Об'єктна модель документа (Document Object Model)";
+// h1.after(ppp);
 
-// !18 ----------------------------------------------------------------------
+// // !18 ----------------------------------------------------------------------
 // const newElLi = document.createElement("li");
 // const elementP = document.createElement("p");
 // elementP.textContent = "Ще один спосіб створити DOM-елементи і помістити їх в дерево - це використовувати рядки з тегами і дозволити браузеру зробити всю важку роботу";
@@ -109,11 +109,101 @@ h1.after(ppp);
 // newElLi.append(h3Element, elementP);
 // list.append(newElLi) //один спосіб додавання розмітки
 
-// !19 -------------------------------------------------------------------------
-const markUp = "<li><h3>Властивість innerHTML</h3><p>Ще один спосіб створити DOM-елементи і помістити їх в дерево - це використовувати рядки з тегами і дозволити браузеру зробити всю важку роботу </p></li>";
-list.insertAdjacentHTML("beforeend", markUp); //інший спосіб додавання розмітки
+// // !19 -------------------------------------------------------------------------
+// const markUp = "<li><h3>Властивість innerHTML</h3><p>Ще один спосіб створити DOM-елементи і помістити їх в дерево - це використовувати рядки з тегами і дозволити браузеру зробити всю важку роботу </p></li>";
+// list.insertAdjacentHTML("beforeend", markUp); //інший спосіб додавання розмітки
 
-// !20 -------------------------------------------------------------------------
-list.innerHTML = "";
+// // !20 -------------------------------------------------------------------------
+// list.innerHTML = "";
 
 // ! ====================================================================================================
+
+// Створіть контейнер div (з класом number-container) в HTML-документі
+
+// const div = document.querySelector(".number-container");
+
+// // console.log(div);
+// // та динамічно створіть 100 блоків (з класом number) наповнивши їх рандомними
+// // числами від 1 до 100 і додайте їх до контейнера div(numberContainer).
+
+// const arrElements = [];
+// const randomNumber = () => Math.floor(Math.random() * 100) + 1;
+// for (let i = 0; i < 100; i++) {
+//     const capibara = document.createElement("div");
+//     capibara.classList.add("number");
+//     const number = randomNumber();
+//     capibara.textContent = number;
+//     if (number % 2 === 0) {
+//         capibara.classList.add("even");
+//     } else {
+//         capibara.classList.add("odd");
+//     }
+//     arrElements.push(capibara);
+// }
+
+// div.append(...arrElements);
+
+// Парні числа повинні мати зелений фон (додати клас even),
+// Непарні числа - жовтий фон (додати клас odd).
+
+// ! =========================================================================
+
+// Form Events, Input, Focus, Blur and Submit.
+
+// Використовуй шаблон форми з файлу html.
+
+// 1 - При події `input`, якщо користувач ввів в поле більше
+// 6 символів то додати клас `success`. Якщо ж символів менше аніж 6,
+// то клас `error`
+
+const input = document.querySelector(".js-username-input");
+// ! -----------------------------------------------------------
+// input.addEventListener("input", handlerInput);
+// function handlerInput() {
+//     const value = input.value.length;
+//     input.classList.toggle("success", value >= 6);
+//     input.classList.toggle("error", value < 6);
+// }
+// console.log(input);
+// ! -------------------------------------------------------------
+
+
+// 2 - При події `focus` зроби перевірку на пустоту поля інпута,
+// якщо ж поле пусте, то зроби `outline` => `'3px solid red'`,
+// якщо при фокусі поле непусте, то `outline` => `'3px solid green'`
+
+// input.addEventListener("focus", handlerInput);
+// function handlerInput() {
+//     const value = input.value.length;
+//     if (value === 0) {
+//         input.style.outline = `3px solid red`;
+//     } else {
+//         input.style.outline = `3px solid green`;
+//     }
+// }
+// ! --------------------------------------------------------------
+
+// 3 - При події `blur` зроби перевірку на пустоту поля інпута,
+// якщо ж поле пусте, то зроби `outline` => `'3px solid red'`,
+// якщо при фокусі поле непусте, то `outline` => `'3px solid lime'`
+
+input.addEventListener("blur", (event) => {
+    const value = event.target.value.trim();
+    if (value === "") {
+        event.target.outline = `3px solid red`;
+    } else {
+        event.target.outline = `3px solid green`;
+    }
+});
+
+// 4 - При події `submit`. Відміни поведінку браузера по змовчуванню.
+// Дістань данні з інпуту і чек боксу, зроби перевірку,
+// що інпут не порожній, також, що нажатий чек бокс у положення true,
+// якщо користувач все виконав вірно, збери данні (userName)
+// у обьект і виведи у консоль. У разі, якщо користувач не виконав
+// одну із умов, виведи повідомлення. Також при події інпут реалізуй додавання
+// ім`я користувача у span, замість слова "Anonymous".
+// Якщо користувач ввів ім`я, а потім видалив, зроби так,
+// щоб на місце повернулось дефолтне знаяення "Anonymous".
+// При відправці форми, очисти інпут, верни чек бокс у положення
+// false, верни дефолтне значення "Anonymous" у span.
