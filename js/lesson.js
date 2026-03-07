af8a41a2843b110066f69c4c1a0db0c858ab5369
maryana-frydrak <frydra4ka@gmail.com>
2026-03-07 18:42:06 +0200

    add lesson.js

---
+++ js/lesson.js
@@ -0,0 +1,16 @@
+// Функція для промісів
+
+// function createPromise(value, delay, isPositive) {
+//     const promise = new Promise((res, rej) => {
+//         setTimeout(() => {
+//             if (isPositive) {
+//                 res(value);
+//             } else {
+//                 rej(value);
+//             }
+//         }, delay);
+//     });
+//     return promise;
+// }
+
+// !====================================================================
